7597448cadfef470496beee673493247e8aab0d3
hys <nemona_01@naver.com>
2022-02-23 17:58:13 +0900

    이미지 뷰어

--- cSharp_imgViewer/Form1.Designer.cs
+++ cSharp_imgViewer/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(545, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(12, 12);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(376, 21);
+            this.result.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(376, 517);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(394, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(545, 515);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(951, 579);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- cSharp_imgViewer/Form1.cs
+++ cSharp_imgViewer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace cSharp_imgViewer
 {
@@ -15,6 +16,101 @@ namespace cSharp_imgViewer
         public Form1()
         {
             InitializeComponent();
+            this.button1.Click += button1_Click;
+
+        }
+
+        // find 버튼 및 폴더경로 textbox로 넣음
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string fileContent;
+            string filePath;
+            string fileName;
+            string dirPath;
+            result.Text = null;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; | All files (*.*) | *.*; ";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = openFileDialog.SafeFileName;
+
+                    filePath = openFileDialog.FileName;
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+
+                    dirPath = filePath.Replace(fileName, "");
+
+                    result.Text = dirPath;
+                }
+            }
+
+            makeList();
+
+        }
+
+        public void makeList()
+        {
+            string folderName;
+            string[] arr = new string[100];
+            ListViewItem itm;
+            int idx = 0;
+
+            listView1.Items.Clear();
+
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("이름", 100);
+            listView1.Columns.Add("크기", 60);
+            listView1.Columns.Add("날짜", 500);
+
+            folderName = result.Text.Substring(0, result.Text.Length - 1);
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(folderName);
+
+            foreach (System.IO.FileInfo file in dir.GetFiles())
+            {
+                if (file.Name.Contains(".jpg") || file.Name.Contains(".png") || file.Name.Contains(".jfif") || file.Name.Contains(".jpeg")
+                    || file.Name.Contains("jpe"))
+                {
+                    arr[idx] = file.Name;
+                    arr[idx + 1] = file.Length.ToString();
+                    arr[idx + 2] = file.CreationTime.ToString();
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                ListView.SelectedListViewItemCollection items = listView1.SelectedItems;
+                ListViewItem lvItem = items[0];
+
+                string Path = result.Text + lvItem.SubItems[0].Text;
+
+                Console.WriteLine(Path);
+
+                Image img = Image.FromFile(Path);
+
+                pictureBox1.Image = img;
+
+              
+            }
         }
     }
 }
